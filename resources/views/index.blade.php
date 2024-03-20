012cf00bc421cbdcb1af064d99b01ba66e33e86f
nghia2742-briswell <nghia.nt01@briswell-vn.com>
2024-03-20 17:02:20 +0700

    refs #126144 Research validation (client validation, server validation, char 1-byte/2-byte, regex expression, etc.): coding validation client vs server

--- resources/views/index.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.main')
-@section('content')
-    <h1>WELCOME!</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
-use App\Models\User;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,47 +9,18 @@ use Illuminate\Support\Facades\DB;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    $id = Auth::id();
-    if (!$id) {
-        return view('index');
-    }
+Route::get('/', function(){
+    return "Welcome";
+});
 
-    $user = DB::table('users')->find($id);
-    return view('index', ['name' => $user->name]);
-})->name('home');
+Route::get('/client', [HomeController::class, 'index']);
 
-// Auth
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/server', [HomeController::class, 'index2']);
 
-Route::post('/login', [AuthController::class, 'handleLogin']);
+Route::post('/handle', [HomeController::class, 'handle']);
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-Route::post('/register', [AuthController::class, 'handleRegister']);
-
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
-
-// Product
-Route::get('/product', [ProductController::class, 'index'])->name('product');
-
-Route::get('/product/{id}', [ProductController::class, 'show']);
-
-Route::get('/add', [ProductController::class, 'add'])->name('product.add');
-
-Route::post('/add', [ProductController::class, 'store']);
-
-Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
-
-Route::put('/edit/{id}', [ProductController::class, 'update']);
-
-Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('product.delete');
-
-// Post
-Route::get('/posts', [PostController::class, 'index']);
