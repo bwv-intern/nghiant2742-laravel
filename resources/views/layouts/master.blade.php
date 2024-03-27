611bb5c1ab372bdc61dbe6c1e4087d4f76781999
nghia2742-briswell <nghia.nt01@briswell-vn.com>
2024-03-27 14:16:44 +0700

    refs #126151 [Admin] Login - link static assets.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,16 +7,16 @@
         <title>@yield('title', config('app.name', '@Master Layout'))</title>
 
         {{-- BS5 --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         @vite(['resources/css/app.css'])
     </head>
     <body>
     
         @yield('content')
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/jquery/3.7.1/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('js/jquery/jquery.validation/jquery.validate.min.js') }}"></script>
         @vite(['resources/js/app.js'])
     </body>
 </html>
